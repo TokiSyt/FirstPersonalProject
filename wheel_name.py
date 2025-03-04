7e753e6b2f846acd3e1558575ee024a9af586a2d
TokiSyt <yuutokei30@gmail.com>
2025-03-04 16:01:55 +0100

    better readability + infinite loop bug fixed

--- wheel_name.py
+++ wheel_name.py
@@ -167,155 +167,127 @@ def newListName(name_amount, list_name):
 
 def get_names():
 
-    while_breaker = True
-
-    while while_breaker:
+    while True:
         try:
             new_list = int(input("\nWhat action do you wish to take?\n\n(1) New/edit list.\n(2) Spin existing list.\n(3) Check existing lists.\n(4) Delete a list. \n(5) Read a list. \n(6) Edit a list name. \n(7) Exit the program.\n\nInsert here: "))
             if new_list in [1, 2, 3, 4, 5, 6, 7]:
                 if new_list == 7:
-                    return None
-                else:
-                    break
+                    return
+                break
         except:
-            print("\nPlease insert a valid number from 1 - 5.") 
+            print("\nPlease insert a valid number from 1 - 7.") 
 
-    while_breaker = True
-    while while_breaker:
-        try:
-            if new_list in [1]:
+
+    if new_list == 1: #new list/overwrite
+        while True:
+            try:
                 name_amount = int(input("\nInsert the pretended amount of names you are going to list: "))
                 if isinstance(name_amount, int):
-                    while_breaker = False
-            else:
-                while_breaker = False
-        except:
-            print("\nInsert a valid number for the amount of names.")
+                    break
+            except:
+                print("\nInsert a valid number for the amount of names.")
 
-    while_breaker = True
-    while while_breaker:
-        try:
-            if new_list in [1]:
-                print_saved_files()
-                list_name = str(input("\nInsert here your list name or the name from the list you wish to edit: "))
-                overwrite_test = list_name + ".json"
-                list_path = os.path.join(LISTS_DIR, overwrite_test)
-                if os.path.exists(list_path):
-                    overwrite_opt = str(input(f'\nA list named "{list_name}" already exists. Do you wish to overwrite the list? (yes)/(no)\nAnswer here: ')).strip().lower()
-                    if overwrite_opt == "no":
+        while True:
+                try:
+                    print_saved_files()
+                    list_name = str(input('\nInsert here your list name or the name from the list you wish to edit or "exit" to leave this section: '))
+                    if list_name.lower() == "exit":
                         get_names()
-                        while_breaker = False
                         break
-                    elif overwrite_opt == "yes":
-                        print(f"\nThe program will run {name_amount} times. Please, insert a singular name and click Enter. A counter will be displayed to show how many spots are left.")
+                    overwrite_test = list_name + ".json"
+                    list_path = os.path.join(LISTS_DIR, overwrite_test)
+                    if os.path.exists(list_path):
+                        overwrite_opt = str(input(f'\nA list named "{list_name}" already exists. Do you wish to overwrite the list? (yes)/(no)\nAnswer here: ')).strip().lower()
+                        if overwrite_opt == "no":
+                            continue
+                        elif overwrite_opt == "yes":
+                            print(f"\nThe program will run {name_amount} times. Please, insert a singular name and click Enter. A counter will be displayed to show how many spots are left.")
+                            newListName(name_amount, list_name)
+                            break
+                        else:
+                            print('Answer "yes" or "no".')
+                    else:
                         newListName(name_amount, list_name)
-                        while_breaker = False
-                newListName(name_amount, list_name)
-                while_breaker = False
-            else:
-                while_breaker = False
+                        break
+                except:
+                    print("\nAn error occurred while creating/overwriting a list.")
 
-        except:
-            print("\nPlease follow all instructions")
-    while_breaker = True
-
-    while while_breaker:
-        if new_list == 2: #spin an existing list
+    elif new_list == 2: #spin an existing list
+        while True:
             try:
                 print_saved_files()
                 list_to_spin = input('\nChoose a list from the list above to spin the names or "exit" to leave the spin.\n\nInsert here: ')
+                if list_to_spin.lower() == "exit":
+                    get_names()
+                    break
+
                 spin_list_name = list_to_spin + ".json"
                 list_to_spin_path = os.path.join(LISTS_DIR, spin_list_name)
-                if list_to_spin == "exit":
-                    while_breaker = False
-                    get_names()
-                    break
+ 
+                if os.path.exists(list_to_spin_path):
+                    spin_the_wheel(list_to_spin)
                 else:
-                    if os.path.exists(list_to_spin_path):
-                        spin_the_wheel(list_to_spin)
-                        get_names()
-                        while_breaker = False
-                        break
-                    else:
-                        print("\nThe list inserted does not exist.")
+                    print("\nThe list inserted does not exist.")
             except:
                 print("An error showed up while starting up the wheel.")
-        else:
-            while_breaker = False
-    while_breaker = True
 
-    if new_list == 3: #see all files
+    elif new_list == 3: #see all files
         print_saved_files()
-        get_names()
 
-    while while_breaker:
-        try:
-                
-            if new_list == 4: #delete a list
+    elif new_list == 4: #delete a list
+        while True:
+            try:   
                 print_saved_files()
                 list_t_delet = input('\nPlease insert the name of the list you wish to delete or "exit" to go out of the deleting mode.\n\nInsert here: ')
-                if list_t_delet == "exit":
-                    while_breaker = False
+                if list_t_delet.lower() == "exit":
                     get_names()
                     break
+
                 list_td = list_t_delet + ".json"
                 list_path = os.path.join(LISTS_DIR, list_td)
+
                 if os.path.exists(list_path):
                     delete_list(list_td)
-            else:
-                while_breaker = False
-        except:
-            print("\nAn error occured while trying to choose a list to delete. Try again.")
-    while_breaker = True
+                else:
+                    print("\nThe list inserted does not exist.")
+            except:
+                print("\nAn error occured while trying to choose a list to delete. Try again.")
 
-    while while_breaker:
-        try:
-            if new_list == 5: #read list content
+    elif new_list == 5: #read list content
+        while True:
+            try:
                 print_saved_files()
                 user_input = input('\nPlease insert the name of the list you wish to read or "exit" to go out of the deleting mode.\n\nInsert here: ')
-                if user_input == "exit":
-                    while_breaker = False
+                if user_input.lower() == "exit":
                     get_names()
                     break
 
                 list_t_read = user_input + ".json" 
                 print_file(list_t_read)
-                get_names()
-                while_breaker = False
-            else:
-                while_breaker = False
-        except:
-            print("\nAn error occurred while trying to read a file. Try again.")
-    while_breaker = True
 
+            except:
+                print("\nAn error occurred while trying to read a file. Try again.")
 
-    while while_breaker:
-        try:
-            if new_list == 6:
+    elif new_list == 6:
+        while True:
+            try:
                 print_saved_files()
                 list_name_input = input('\nPlease insert the name of the list you wish to edit the name or "exit" to go out of the deleting mode.\n\nInsert here: ')
-
-                if list_name_input == "exit":
-                    while_breaker = False
+                if list_name_input.lower() == "exit":
                     get_names()
                     break
 
                 list_to_edit = list_name_input + ".json"
                 path_test = os.path.join(LISTS_DIR, list_to_edit)
+
                 if os.path.exists(path_test):
                     new_list_name = input('\nPlease insert the new name of the selected list.\n\nInsert here: ')
                     new_list_name_c = new_list_name + ".json"
                     edit_list_name(list_to_edit, new_list_name_c)
                 else:
                     print("\nList not found.")
-                    get_names()
-                    while_breaker = False
-                    break
-            else:
-                while_breaker = False
-        except:
-            print("\nChosen file not found. Try again.")
-    while_breaker = True
+            except:
+                print("\nChosen file not found. Try again.")
 
 
 def main():
