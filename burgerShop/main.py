578c3a7d27ef56705af61b29fe15d970f20ba75a
TokiSyt <yuutokei30@gmail.com>
2025-03-09 14:25:28 +0100

    Adding inside and drive through windows + images

--- burgerShop/main.py
+++ burgerShop/main.py
@@ -1,56 +1,74 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-#self-note, tkinter does not support gifs properly
+#Self-note, tkinter does not support gifs properly
 #----------------------------------------------------------------------------#
 
-#IMAGES:
+#IMAGES & GIFS:
 
 SHOP_ENTRANCE = "images/entrance.gif"
 SHOP_ICON = "images/burgerIcon.ico"
+INSIDE = "images/inside.jpg"
+DRIVE_GIF = "images/drive.gif"
 
 #----------------------------------------------------------------------------#
+#basic infos
 
-#window
+def switchFrame(frame):
+    frame.tkraise()
+
+#Window
 root = tk.Tk()
 width = 1080
 heigth = 720
 root.geometry(f"{width}x{heigth}")
 
-#title & icon
+#Images
+speedMs = 100
+entranceGif = Image.open(SHOP_ENTRANCE)
+insidePhoto = Image.open(INSIDE).resize((1080, 720), Image.Resampling.LANCZOS)
+insidePhoto = ImageTk.PhotoImage(insidePhoto)
+driveGif = Image.open(DRIVE_GIF)
+
+#Title & icon
 root.iconbitmap(default=SHOP_ICON)
 root.title('Burger Shop')
 
-#gifs
-entrance_gif = Image.open(SHOP_ENTRANCE)
+#Frames for window switch
+entranceMenu = tk.Frame(root)
+insideShop = tk.Frame(root)
+driveMenu = tk.Frame(root)
 
-#frames
-frames_list = []
+for frame in (entranceMenu, insideShop, driveMenu):
+    frame.place(x=0, y=0, relheight=1, relwidth=1)
+
+#----------------------------------------------------------------------------#
+#Entrance 
+
+entFramesList = []
 try:
     while True:
-        single_frame = entrance_gif.copy().resize((width, heigth)) # ".resize()"
-        frames_list.append(ImageTk.PhotoImage(single_frame)) 
-        entrance_gif.seek(len(frames_list)) 
+        single_frame = entranceGif.copy().resize((width, heigth)) # ".resize()"
+        entFramesList.append(ImageTk.PhotoImage(single_frame)) 
+        entranceGif.seek(len(entFramesList)) 
 except EOFError: #EOF (end of frames)
     pass
 
-frame_count = len(frames_list)
-frame_index = 0
+entFrameCount = len(entFramesList)
+entFrameIndex = 0
 
-#Entrance animation
-entrance_label = tk.Label(root, image=frames_list[0])
+
+entrance_label = tk.Label(entranceMenu, image=entFramesList[0])
 entrance_label.pack()
 
-def update_gif():
-    global frame_index
-    speed_ms = 100
+def update_entrance_gif():
+    global entFrameIndex
+    global speedMs
 
-    entrance_label.config(image=frames_list[frame_index])
-    frame_index = (frame_index + 1) % frame_count # index % total count = 0 
-    root.after(speed_ms, update_gif)
+    entrance_label.config(image=entFramesList[entFrameIndex])
+    entFrameIndex = (entFrameIndex + 1) % entFrameCount # last index % total count = 0 
+    root.after(speedMs, update_entrance_gif)
 
-    
-#button_test
 #bg - button color
 #fg - text color
 
@@ -62,40 +80,103 @@ font = "Courier"
 fontSize = 12
 
 enterButton = tk.Button(
-    root, width=20, height=3, 
+    entranceMenu, width=20, height=3, 
     bg=bgColor, fg=fgColor, 
     text="╔════════╗\n║  ENTER  ║\n╚════════╝", font=(font, fontSize), 
-    command=lambda: print("Enter button")
+    command=lambda: switchFrame(insideShop)
 )
 enterButton.place(x=first_x, y=first_y*1.25)
 
 driveThroughButton = tk.Button(
-    root, width=20, height=3, 
+    entranceMenu, width=20, height=3, 
     bg=bgColor, fg=fgColor, 
     text="╔════════════════╗\n║  DRIVE THROUGH  ║\n╚════════════════╝", font=(font, fontSize), 
-    command=lambda: print("Drive through button")
+    command=lambda: switchFrame(driveMenu)
 )
 driveThroughButton.place(x=first_x, y=first_y*2.5)
 
 creditsButton = tk.Button(
-    root, width=20, height=3, 
+    entranceMenu, width=20, height=3, 
     bg=bgColor, fg=fgColor, 
     text="╔══════════╗\n║  CREDITS  ║\n╚══════════╝", font=(font, fontSize), 
-    command=lambda: print("credits button")
+    command=lambda: print("Credits button")
 )
 creditsButton.place(x=first_x, y=first_y*3.75)
 
 exitButton = tk.Button(
-    root, width=20, height=3, 
+    entranceMenu, width=20, height=3, 
     bg=bgColor, fg=fgColor, 
     text="╔═══════╗\n║  EXIT  ║\n╚═══════╝", font=(font, fontSize), 
     command=lambda: root.destroy()
 )
-
 exitButton.place(x=first_x, y=first_y*5)
 
+update_entrance_gif()
 
-update_gif()
+#----------------------------------------------------------------------------#
+
+#Inside
+
+insideLabel = tk.Label(insideShop, image=insidePhoto)
+insideLabel.pack()
+
+backButton = tk.Button(
+    insideShop,
+    text="╔══════════╗\n║ Return to the entrance ║\n╚══════════╝",
+    bg="#b03a2e",
+    fg=fgColor,
+    borderwidth=0,
+    width=21,
+    height=3,
+    command=lambda: switchFrame(entranceMenu)
+)
+backButton.place(x=first_x, y=heigth - 80)
+
+#----------------------------------------------------------------------------#
+
+#Drive Through
+
+driveFramesList = []
+try:
+    while True:
+        single_frame = driveGif.copy().resize((width, heigth)) # ".resize()"
+        driveFramesList.append(ImageTk.PhotoImage(single_frame)) 
+        driveGif.seek(len(driveFramesList)) 
+except EOFError: #EOF (end of frames)
+    pass
+
+driveFrameCount = len(driveFramesList)
+driveFrameIndex = 0
+
+
+driveLabel = tk.Label(driveMenu, image=driveFramesList[0])
+driveLabel.pack()
+
+def update_drive_gif():
+    global driveFrameIndex
+    global speedMs
+
+    driveLabel.config(image=driveFramesList[driveFrameIndex])
+    driveFrameIndex = (driveFrameIndex + 1) % driveFrameCount # last index % total count = 0 
+    root.after(speedMs, update_drive_gif)
+
+backDriveButton = tk.Button(
+    driveMenu,
+    text="╔══════════╗\n║ Return to the entrance ║\n╚══════════╝",
+    bg="#f5b7b1",
+    fg="#a93226",
+    borderwidth=0,
+    width=21,
+    height=3,
+    command=lambda: switchFrame(entranceMenu)
+)
+backDriveButton.place(x=first_x, y=heigth - 80)
+
+update_drive_gif()
+
+#----------------------------------------------------------------------------#
+
+switchFrame(entranceMenu)
 
 root.mainloop()
 
